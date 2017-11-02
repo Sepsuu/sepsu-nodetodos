25b1e0ca9f4b1ec0ea2ac0d59b6a9e70e792ab43
Severi Peltonen <severi.peltonen@gmail.com>
2017-11-02 21:18:40 +0200

    Test and develoment envs

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -163,6 +163,7 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text);
                 expect(res.body.todo.completed).toBe(true);
+                // expect to be a number ei toimi fuufuu
 
             })
             .end(done);
